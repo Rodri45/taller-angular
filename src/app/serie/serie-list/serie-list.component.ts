325fe94d54c7634e6c06c9417111b508aa086eb0
Rodrigo Garcia <r.garciaa2@uniandes.edu.co>
2024-11-10 00:51:44 -0500

    funciona

--- src/app/serie/serie-list/serie-list.component.ts
+++ src/app/serie/serie-list/serie-list.component.ts
@@ -1,27 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from '../serie';
 import { SerieService } from './serie.service';
-
 @Component({
   selector: 'app-serie-list',
   templateUrl: './serie-list.component.html',
   styleUrls: ['./serie-list.component.css']
 })
 export class SerieListComponent implements OnInit {
-
   series: Array<Serie> = [];
-
   seasonsAvg: number = 0;
-
   constructor(private serieService: SerieService) { }
-
   getSeries(): void {
     this.serieService.getSeries().subscribe(series => {
       this.series = series;
       this.calculateSeasonsAvg();
     });
   }
-
   calculateSeasonsAvg(): void {
     let sum: number = 0;
     for (let serie of this.series) {
@@ -30,7 +24,6 @@ export class SerieListComponent implements OnInit {
     }
     this.seasonsAvg = sum / this.series.length;
   }
-
   ngOnInit() {
     this.getSeries();
   }
--- src/app/serie/serie-list/serie.service.spec.ts
+++ src/app/serie/serie-list/serie.service.spec.ts
@@ -1,15 +1,11 @@
-/* tslint:disable:no-unused-variable */
-
 import { inject, TestBed } from '@angular/core/testing';
 import { SerieService } from './serie.service';
-
 describe('Service: Serie', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [SerieService]
     });
   });
-
   it('should ...', inject([SerieService], (service: SerieService) => {
     expect(service).toBeTruthy();
   }));
