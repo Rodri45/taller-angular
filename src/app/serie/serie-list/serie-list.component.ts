6d83b404810cfe2916dacaad1aa47eea31afec3f
Rodrigo Garcia <r.garciaa2@uniandes.edu.co>
2024-11-10 01:25:38 -0500

    final

--- src/app/serie/serie-list/serie-list.component.ts
+++ src/app/serie/serie-list/serie-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from '../serie';
-import { SerieService } from './serie.service';
+import { SerieService } from '../serie.service';
 @Component({selector: 'app-serie-list', templateUrl: './serie-list.component.html',styleUrls: ['./serie-list.component.css']})
 export class SerieListComponent implements OnInit {
   series: Array<Serie> = [];
--- src/app/serie/serie.service.ts
+++ src/app/serie/serie.service.ts
@@ -2,8 +2,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
-import { Serie } from '../serie';
+import { environment } from '../../environments/environment.development';
+import { Serie } from './serie';
 @Injectable({
   providedIn: 'root'
 })
